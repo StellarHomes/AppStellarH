81178aa1b8f010e362463af933b285b28adf7935
Duvan1119P <duvan.p.r117@gmail.com>
2025-04-14 16:58:41 -0500

    Primer commit

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: "192.168.0.3",
   user: "root",
   password: "duvan1918",
-  database: "stellar_homes",
+  database: "stellar_homess",
 });
 
 connection.connect((err) => {
